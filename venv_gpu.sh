9846bab30faad4be49eda356317c9a465a5dc61c
Amithabh A <amithabh2002@gmail.com>
2024-07-08 19:45:05 +0530

    Update venv_gpu.sh

--- venv_gpu.sh
+++ venv_gpu.sh
@@ -1,5 +1,7 @@
 python3 -m venv venv
 source venv/bin/activate
+git clone https://github.com/pytorch/csprng.git
+git clone https://github.com/Amithabh-A/CryptGPU.git
 cd csprng
 pip install torch
 python3 setup.py install
